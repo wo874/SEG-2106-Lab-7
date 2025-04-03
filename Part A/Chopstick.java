a1a87615b43ce2c3aadd5274424bc6d2be0d0ca1
wo874 <92176181+wo874@users.noreply.github.com>
2025-04-03 17:51:36 -0400

    Update Chopstick.java

--- Part A/Chopstick.java
+++ Part A/Chopstick.java
@@ -1,1 +1,32 @@
+public class Chopstick {
+  
+	private int ID;
+	private boolean free;
 
+	Chopstick(int ID) {
+    
+		this.ID=ID;
+		this.free=true;
+	}
+	
+	synchronized void take() {
+    
+		while (!free) {
+			try{
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		free = false;
+	}
+	
+	synchronized void release() {
+		free=true;
+		notify();
+	}
+	    
+	public int getID() {
+	    return(ID);
+	}
+}
