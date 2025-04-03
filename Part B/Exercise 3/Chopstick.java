df265f7d0de48438f4392ce5833bbd26cc7076fc
wo874 <92176181+wo874@users.noreply.github.com>
2025-04-03 18:14:38 -0400

    Update Chopstick.java

--- Part B/Exercise 3/Chopstick.java
+++ Part B/Exercise 3/Chopstick.java
@@ -1,1 +1,37 @@
+public class Chopstick {
+	private int ID;
+	private boolean free;
 
+	Chopstick(int ID) {
+    
+		this.ID = ID;
+		this.free = true;
+	}
+	
+	synchronized boolean Take(int waitTime) {
+    
+        long startTime = System.currentTimeMillis();
+        while (!free) {
+            try {
+                wait(waitTime);
+              
+                if (System.currentTimeMillis() - startTime >= waitTime) {
+                    return false;
+                }
+            } catch (InterruptedException e) {
+                System.out.print(e);
+            }
+        }
+        free = false;
+        return true;
+    }
+	
+	synchronized void release() {
+		free = true;
+		notify();
+	}
+	    
+	public int getID() {
+	    return(ID);
+	}
+}
